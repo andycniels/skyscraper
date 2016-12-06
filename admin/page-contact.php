bbbb4e07b0b58e16c161636d7614c8b67965230e
Anders Cramer Nielsen <anders.cramer@me.com>
2016-12-06 16:10:57 +0100

    page contact igen

--- admin/page-contact.php
+++ admin/page-contact.php
@@ -3,15 +3,9 @@
 if (isset($_POST["contact"])){
     $street = filter_input(INPUT_POST, 'street');
     $town = filter_input(INPUT_POST, 'town');
-    $phone = filter_input(INPUT_POST, 'phone', FILTER_VALIDATE_INT);
+    $phone = filter_input(INPUT_POST, 'phone');
     $email = filter_input(INPUT_POST, 'email');
     
-    if (filter_var($phone, FILTER_VALIDATE_INT) === false) {
-        $phoneVal = '<p style="color:red;">Not a real number</p>';
-    }
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-        $emailVal = "<p style='color:red;'>'$email' is not a valid email address</p>";
-    }
     if(empty($_POST["street"])){
         $error = '<p style="color:red;">Street name must not be empty</p>';
     }
@@ -21,17 +15,28 @@ if (isset($_POST["contact"])){
     else if(empty($_POST["phone"])){
         $error3 = '<p style="color:red;">Phone nr. must not be empty</p>';
     }
+    else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        $emailVal = "<p style='color:red;'>'$email' is not a valid email address</p>";
+    }
     else if(empty($_POST["email"])){
         $error4 = '<p style="color:red;">Email must not be empty</p>';
     }else{
-        echo $street;
-        echo $town;
-        echo $phone;
-        echo $email;
+        require_once '../dbcon.php';
+        $sql = 'UPDATE page_contact SET street=?,town=?,phone=?,email=? WHERE page_contact_id=1';
+        $stmt = $link->prepare($sql);
+        $stmt->bind_param('ssss', $street, $town, $phone, $email);
+        $stmt->execute();
+        header("Refresh:0");
     }
 }
 include 'header.php';
 include 'nav.php';
+require_once '../dbcon.php';
+$stmt = $link->prepare("SELECT street, town, phone, email FROM `page_contact`");
+    $stmt->execute();
+    $stmt->bind_result($street, $town, $phone, $email);
+    while($stmt->fetch()) {	
+    }
 ?>
 <!-- page content -->
 <div class="right_col main_place" role="main">
@@ -42,21 +47,21 @@ include 'nav.php';
             <form action="<?php REQUEST_FILENAME ?>" method="POST">
                 <div class="form-group">
                     <?php echo $error; ?>
-                    <input type="text" class="form-control" name="street" placeholder="Street">
+                    <input type="text" class="form-control" name="street" value="<?= $street ?>" placeholder="Street">
                 </div>
                 <div class="form-group">
                     <?php echo $error2; ?>
-                    <input type="text" class="form-control" name="town" placeholder="zip and town">
+                    <input type="text" class="form-control" name="town" value="<?= $town ?>" placeholder="zip and town">
                 </div>
                 <div class="form-group">
                     <?php echo $error3; ?>
                     <?php echo $phoneVal; ?>
-                    <input type="text" class="form-control" name="phone" placeholder="Phone">
+                    <input type="text" class="form-control" name="phone" value="<?= $phone ?>" placeholder="Phone">
                 </div>
                 <div class="form-group">
                     <?php echo $error4; ?>
                     <?php echo $emailVal; ?>
-                    <input type="text" class="form-control" name="email" placeholder="Email">
+                    <input type="text" class="form-control" name="email" value="<?= $email ?>" placeholder="Email">
                 </div>
                 <input class="btn btn-default" name="contact" type="submit" value="Update">
             </form>
