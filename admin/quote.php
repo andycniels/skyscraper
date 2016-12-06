48b34ab1fefebe02f530f480e88a7f8177b80023
Casper Ragn <cr@simplar.dk>
2016-12-06 11:13:58 +0100

    oprettet php i quote

--- admin/quote.php
+++ admin/quote.php
@@ -1,7 +1,31 @@
 <?php
+//Update
+if (isset($_POST["about"])){ 
+    $quote = filter_input(INPUT_POST, 'quote');
+
+    if(empty($_POST["quote"])){
+        $error = '<p style="color:red;">Must not be empty</p>';
+    }else{
+        require_once '../dbcon.php';
+        $sql = 'UPDATE about SET quote=? WHERE about_id=1';
+        $stmt = $link->prepare($sql);
+        $stmt->bind_param('s', $quote);
+        $stmt->execute();
+        header("Refresh:0");
+    }
+}
 include 'header.php';
 include 'nav.php';
+require_once '../dbcon.php';
+$stmt = $link->prepare("SELECT quote FROM `about`");
+    $stmt->execute();
+    $stmt->bind_result($quote);
+    while($stmt->fetch()) {	
+    }
+
 ?>
+
+
 <!-- page content -->
 <div class="right_col main_place" role="main">
     <div class="row">
@@ -10,11 +34,13 @@ include 'nav.php';
             <p>Quibusdam ne eiusmod, probant esse nescius aut dolore commodo ita fidelissimae, 
 								quibusdam iis anim. Nulla laborum incididunt, fore est ea elit doctrina. 
             </p>
-            <form>
+            <form action="<?php REQUEST_FILENAME ?>" method="POST">
             <div class="form-group">
-				<textarea class="form-control" id="text" placeholder="Text"></textarea>
+             <?php echo $error; ?>
+				<textarea class="form-control" name="quote" placeholder="Text">
+<?php echo $quote; ?></textarea>
             </div>
-				<button type="submit" class="btn btn-default">Submit</button>
+				 <input class="btn btn-default" name="about" type="submit" value="Update">
             </form>
         </div>
     </div>
