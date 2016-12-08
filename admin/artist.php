f059bb8be5a447e837fd46bcef5369e6017d5f13
Anders Cramer Nielsen <anders.cramer@me.com>
2016-12-08 21:33:52 +0100

    see artist and contact

--- admin/artist.php
+++ admin/artist.php
@@ -13,35 +13,53 @@ include 'nav.php';
                 <tr>
                     <th>Img</th>
                     <th>Band name</th>
-                    <th>Description</th>
                     <th>Label</th>
                     <th>Genre</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
+                    <th>Edit band details</th>
+                    <th>Edit band image</th>
+                    <th>Edit artist contact info</th>
+                    <th>Delete band and contact</th>
                 </tr>
+                <?php
+                require_once '../dbcon.php';
+                $stmt = $link->prepare("SELECT m.music_id, 
+                                               m.band_name, 
+                                               m.img, 
+                                               m.text, 
+                                               m.fk_musicgenre_id, 
+                                               m.fk_label_id, 
+                                               m.fk_artistcontact_id, 
+                                               m.fk_cat_id,
+                                               l.label_id,
+                                               l.label,
+                                               g.genre_id,
+                                               g.genre
+                                               
+                                               FROM music m, labels l, music_genre g 
+                                               WHERE m.fk_musicgenre_id = g.genre_id
+                                               AND m.fk_label_id = l.label_id
+                                        ");
+                $stmt->execute();
+                $stmt->bind_result($mid, $bn, $img, $text, $fkmid, $fklid, $fkacid, $fkcid,
+                                   $lid, $label,
+                                   $gid, $genre
+                                  );
+                
+                while($stmt->fetch()) {
+                ?>
                 <tr>
-                    <td>
-                        <img src="../img/jakob_d.jpg";>
-                    </td>
-                    <td>
-                        <p>Jazz band</p>
-                    </td>
-                    <td>
-                        <p>Appellat enim ita eiusmod praesentibus. Eram iis nescius, multos laboris ad 
-                            senserit. Nam esse exquisitaque, se nam sunt quid fugiat, excepteur summis 
-                            voluptate mandaremus quo ad in quae minim quem. Possumus non eiusmod, est illum 
-                            malis ita appellat ne anim arbitror a occaecat hic iudicem enim offendit 
-                            senserit.</p>
-                    </td>
-                    <td>
-                        <p>Skyscraper</p>
-                    </td>
-                    <td>
-                        <p>pop</p>
-                    </td>
-                    <td><a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
-                    <td><a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td>
+                    <td><img src="../img/artist/<?= $img ?>" class='img-responsive'></td>
+                    <td><?= $bn ?></td>
+                    <td><?= $label ?></td>
+                    <td><?= $genre ?></td>
+                    <td><a href="edit?mid=<?= $mid ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                    <td><a href="edit?img=<?= $mid ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                    <td><a href="edit?acid=<?= $fkacid ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                    <td><a href="delete?mid=<?= $mid ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td>
                 </tr>
+                <?php
+                }
+                ?>
             </table>
         </div>
     </div>
---
+++ admin/delete.php
@@ -0,0 +1,8 @@
+<?php
+$mid = $_GET['mid'];
+
+if(isset($_GET['mid'])){
+    echo 'Her sletter man et band og artist contact <br>';
+    echo $mid;  
+}
+?>
