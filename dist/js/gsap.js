38ef0c7816c71b617641953d509a5bf5c1154d55
Aschultz <andreasschultz@hotmail.com>
2016-12-09 10:05:47 +0100

    Katalog

---
+++ dist/js/gsap.js
@@ -0,0 +1,31 @@
+$(".overlay:nth-child(1), .overlay:nth-child(1)").mouseenter(function(){
+			var tween = TweenMax.to('.overlay:nth-child(1)', 1, {css:{opacity:'1'}, ease:Power4.easeOut});
+        });
+
+        $(".overlay:nth-child(1), .overlay:nth-child(1)").mouseleave(function(){
+        	var tween = TweenMax.to('.overlay:nth-child(1)', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+        });
+
+$(".artist:nth-child(2), .artist:nth-child(2)").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(2)', 1, {css:{opacity:'0'}, ease:Power4.easeOut});
+        });
+
+        $(".artist:nth-child(2), .artist:nth-child(2)").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(2)', 1, {css:{opacity: '1'}, ease:Power4.easeOut});
+        });
+
+$(".artist:nth-child(3), .artist:nth-child(3)").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(3)', 1, {css:{opacity:'0'}, ease:Power4.easeOut});
+        });
+
+        $(".artist:nth-child(3), .artist:nth-child(3)").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(3)', 1, {css:{opacity: '1'}, ease:Power4.easeOut});
+        });
+
+$(".artist:nth-child(4), .artist:nth-child(4)").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(4)', 1, {css:{opacity:'0'}, ease:Power4.easeOut});
+        });
+
+        $(".artist:nth-child(4), .artist:nth-child(4)").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(4)', 1, {css:{opacity: '1'}, ease:Power4.easeOut});
+        });
