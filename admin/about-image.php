6b4f276c077ce2de32da5781c96e57138d78d390
Anders Cramer Nielsen <anders.cramer@me.com>
2016-12-09 12:00:21 +0100

    Search function in bands and contact works now

--- admin/about-image.php
+++ admin/about-image.php
@@ -1,4 +1,53 @@
 <?php
+if (isset($_POST["upload"])){
+     //make input ready
+    $img = basename($_FILES["fileToUpload"]["name"]);
+    $img = boxone . '.' . end(explode(".",$_FILES["fileToUpload"]["name"]));
+    
+    //delete img from folder first
+    unlink('../img/boxone.jpg');
+    //upload img to folder
+    $target_dir = "../img/";
+    $target_file = $target_dir . $img;
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]); 
+    //img validation
+    //if empty
+    if(empty(basename($_FILES["fileToUpload"]["name"]))){
+        $img_error = '<p style="color:red;">Must not be empty</p>';
+    }
+    //Check if image file is a actual image or fake image  
+    else if($check == false) {
+        //File is an image;
+        $img_error2 = '<p style="color:red;">File is not an image.</p>';
+        $uploadOk = 0;
+    }
+    // Check if file already exists
+    else if (file_exists($target_file)) {
+        $img_error3 = '<p style="color:red;">File already exists.</p>';
+        $uploadOk = 0;
+    }
+    // Check file size
+    else if ($_FILES["fileToUpload"]["size"] > 5000000) {
+        $img_error4 = '<p style="color:red;">Your file is too large. max 0,5 GB</p>';
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
+        $img_error5 = '<p style="color:red;">Only jpg, jpeg, png files are allowed.</p>';
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    else if ($uploadOk == 0) {
+        $img_error6 = '<p style="color:red;">Your Image was not uploaded, try again.</p>';
+    // if everything is ok, try to upload file  
+    }else{
+        //upload to folder
+        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+        header("Refresh:0");
+    }
+}
 include 'header.php';
 include 'nav.php';
 ?>
@@ -11,14 +60,21 @@ include 'nav.php';
             <p>Quibusdam ne eiusmod, probant esse nescius aut dolore commodo ita fidelissimae, 
                 quibusdam iis anim. Nulla laborum incididunt, fore est ea elit doctrina. 
             </p>
-            <img class="img-responsive" src="http://placehold.it/300x300">
-            <form>
+            <img class="img-responsive" src="../img/boxone.jpg">
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
+                <?php echo $img_error; ?>
+                <?php echo $img_error2; ?>
+                <?php echo $img_error3; ?>
+                <?php echo $img_error4; ?>
+                <?php echo $img_error5; ?>
+                <?php echo $img_error6; ?>
+                <?php echo $img_error7; ?>
                 <div class="form-group">
                     <img class="input-thumb" id="thumbnail">
-                    <input type='file' onchange="readURL(this);" />
+                    <input type="file" name="fileToUpload" id="fileToUpload" value="focus.jpg" onchange="readURL(this);">
                 </div>
                 <br>
-                <button type="submit" class="btn btn-default">Upload</button>
+                <input class="btn btn-default" name="upload" type="submit" value="upload">
             </form>
         </div>
     </div>
--- admin/index.php
+++ admin/index.php
@@ -8,10 +8,112 @@ include 'nav.php';
 <div class="right_col main_place" role="main">
     <div class="row">
         <div class="col-md-8 col-lg-push-2">
-            <?php include 'search.php'; 
-            echo $search;
+            <?php include 'search.php';  ?>
+            <h1 class="page-header">Skyscraper</h1> 
+            <?php  
+                if (!empty($search)) {
+                ?>
+                <h3>Artists</h3>
+                <table class="table table-striped table-hover">
+                    <tr>
+                        <th>Img</th>
+                        <th>Band name</th>
+                        <th>Label</th>
+                        <th>Genre</th>
+                        <th>Edit band details</th>
+                        <th>Edit band image</th>
+                        <th>Edit artist contact info</th>
+                        <th>Delete band and contact</th>
+                    </tr>
+                <?php
+                require_once '../dbcon.php';
+                $stmt = $link->prepare("SELECT m.music_id, 
+                                               m.band_name, 
+                                               m.img, 
+                                               m.text, 
+                                               m.fk_musicgenre_id, 
+                                               m.fk_label_id, 
+                                               m.fk_artistcontact_id, 
+                                               m.fk_cat_id,
+                                               l.label_id,
+                                               l.label,
+                                               g.genre_id,
+                                               g.genre
+                                               
+                                               FROM music m, labels l, music_genre g 
+                                               
+                                               WHERE m.fk_musicgenre_id = g.genre_id
+                                               
+                                               AND m.fk_label_id = l.label_id
+                                               
+                                               AND m.band_name COLLATE UTF8_GENERAL_CI LIKE '%$search%'
+                                        ");
+                $stmt->execute();
+                $stmt->bind_result($mid, $bn, $img, $text, $fkmid, $fklid, $fkacid, $fkcid,
+                                   $lid, $label,
+                                   $gid, $genre
+                                  );
+                
+                while($stmt->fetch()) {
+                ?>
+                    <tr>
+                        <td><img src="../img/artist/<?= $img ?>" class='img-responsive'></td>
+                        <td><?= $bn ?></td>
+                        <td><?= $label ?></td>
+                        <td><?= $genre ?></td>
+                        <td><a href="edit?mid=<?= $mid ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                        <td><a href="edit?img=<?= $mid ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                        <td><a href="edit?acid=<?= $fkacid ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                        <td><a href="delete?mid=<?= $mid ?>"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td>
+                    </tr>
+                <?php
+                }
+                ?>
+                </table>
+                <h3>Artist contact</h3>
+                <table class="table table-striped table-hover">
+                <tr>
+                    <th>Band name</th>
+                    <th>Contact person</th>
+                    <th>Contact phone</th>
+                    <th>Contact email</th>
+                </tr>
+                <?php
+                require_once '../dbcon.php';
+                $stmt = $link->prepare("SELECT a.artist_contact_id, 
+                                               a.contact_name, 
+                                               a.phone, 
+                                               a.email, 
+                                               m.band_name,
+                                               m.fk_artistcontact_id
+                                                   
+                                               FROM artist_contact a, music m
+                                               WHERE m.fk_artistcontact_id = a.artist_contact_id
+                                               AND m.band_name COLLATE UTF8_GENERAL_CI LIKE '%$search%'
+                                               ");
+                $stmt->execute();
+                $stmt->bind_result($acid, $cn, $p, $e, $bn, $fkid);
+                
+                while($stmt->fetch()) {
+                ?>
+                <tr>
+                    <td><?= $bn ?></td>
+                    <td><?= $cn ?></td>
+                    <td><a href="tel:<?= $p ?>"><?= $p ?></a></td>
+                    <td><a href="mailto:<?= $e ?>" target="_top"><?= $e ?></a></td>
+                </tr>
+                <?php
+                }
+                ?>
+                </table>
+                <?php    
+                    
+                    
+                    
+                }else{
+                    echo 'FORSIDEN';
+                }
             ?>
-            <h1 class="page-header">Skyscraper</h1>    
         </div>
     </div>
 </div>
