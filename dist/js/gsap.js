d248d261bd256e973e554b67ac4b51f1cf8a2d34
Aschultz <andreasschultz@hotmail.com>
2016-12-12 09:35:44 +0100

    katalog

--- dist/js/gsap.js
+++ dist/js/gsap.js
@@ -1,31 +1,101 @@
-$(".overlay:nth-child(1), .overlay:nth-child(1)").mouseenter(function(){
-			var tween = TweenMax.to('.overlay:nth-child(1)', 1, {css:{opacity:'1'}, ease:Power4.easeOut});
+$(".artist:nth-child(1) .overlay, .artist:nth-child(1) .overlay").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(1) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut}); 
+            
+            var tween2 = TweenMax.to('.artist:nth-child(1) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
         });
 
-        $(".overlay:nth-child(1), .overlay:nth-child(1)").mouseleave(function(){
-        	var tween = TweenMax.to('.overlay:nth-child(1)', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+        $(".artist:nth-child(1) .overlay, .artist:nth-child(1) .overlay").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(1) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(1) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
         });
 
-$(".artist:nth-child(2), .artist:nth-child(2)").mouseenter(function(){
-			var tween = TweenMax.to('.artist:nth-child(2)', 1, {css:{opacity:'0'}, ease:Power4.easeOut});
+$(".artist:nth-child(1) .artist-info, .artist:nth-child(1) .artist-info").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(1) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut});
+    
+            var tween2 = TweenMax.to('.artist:nth-child(1) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
         });
 
-        $(".artist:nth-child(2), .artist:nth-child(2)").mouseleave(function(){
-        	var tween = TweenMax.to('.artist:nth-child(2)', 1, {css:{opacity: '1'}, ease:Power4.easeOut});
+        $(".artist:nth-child(1) .artist-info, .artist:nth-child(1) .artist-info").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(1) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(1) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
         });
 
-$(".artist:nth-child(3), .artist:nth-child(3)").mouseenter(function(){
-			var tween = TweenMax.to('.artist:nth-child(3)', 1, {css:{opacity:'0'}, ease:Power4.easeOut});
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(".artist:nth-child(2) .overlay, .artist:nth-child(2) .overlay").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(2) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut}); 
+            
+            var tween2 = TweenMax.to('.artist:nth-child(2) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
         });
 
-        $(".artist:nth-child(3), .artist:nth-child(3)").mouseleave(function(){
-        	var tween = TweenMax.to('.artist:nth-child(3)', 1, {css:{opacity: '1'}, ease:Power4.easeOut});
+        $(".artist:nth-child(2) .overlay, .artist:nth-child(2) .overlay").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(2) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(2) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
         });
 
-$(".artist:nth-child(4), .artist:nth-child(4)").mouseenter(function(){
-			var tween = TweenMax.to('.artist:nth-child(4)', 1, {css:{opacity:'0'}, ease:Power4.easeOut});
+$(".artist:nth-child(2) .artist-info, .artist:nth-child(2) .artist-info").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(2) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut});
+    
+            var tween2 = TweenMax.to('.artist:nth-child(2) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
         });
 
-        $(".artist:nth-child(4), .artist:nth-child(4)").mouseleave(function(){
-        	var tween = TweenMax.to('.artist:nth-child(4)', 1, {css:{opacity: '1'}, ease:Power4.easeOut});
+        $(".artist:nth-child(2) .artist-info, .artist:nth-child(2) .artist-info").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(2) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(2) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
+        });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(".artist:nth-child(3) .overlay, .artist:nth-child(3) .overlay").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(3) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut}); 
+            
+            var tween2 = TweenMax.to('.artist:nth-child(3) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
+        });
+
+        $(".artist:nth-child(3) .overlay, .artist:nth-child(3) .overlay").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(3) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(3) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
+        });
+
+$(".artist:nth-child(3) .artist-info, .artist:nth-child(3) .artist-info").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(3) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut});
+    
+            var tween2 = TweenMax.to('.artist:nth-child(3) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
+        });
+
+        $(".artist:nth-child(3) .artist-info, .artist:nth-child(3) .artist-info").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(3) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(3) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
+        });
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(".artist:nth-child(4) .overlay, .artist:nth-child(4) .overlay").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(4) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut}); 
+            
+            var tween2 = TweenMax.to('.artist:nth-child(4) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
+        });
+
+        $(".artist:nth-child(4) .overlay, .artist:nth-child(4) .overlay").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(4) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(4) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
+        });
+
+$(".artist:nth-child(4) .artist-info, .artist:nth-child(4) .artist-info").mouseenter(function(){
+			var tween = TweenMax.to('.artist:nth-child(4) .overlay', 1, {css:{opacity:'0.7'}, ease:Power4.easeOut});
+    
+            var tween2 = TweenMax.to('.artist:nth-child(4) .artist-info', 1, {css:{opacity:'1'}, ease:Power4.easeOut}); 
+        });
+
+        $(".artist:nth-child(4) .artist-info, .artist:nth-child(1) .artist-info").mouseleave(function(){
+        	var tween = TweenMax.to('.artist:nth-child(4) .overlay', 1, {css:{opacity: '0'}, ease:Power4.easeOut});
+            
+            var tween2 = TweenMax.to('.artist:nth-child(4) .artist-info', 1, {css:{opacity:'0'}, ease:Power4.easeOut}); 
         });
