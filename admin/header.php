f27c0c52badba37f0d5974b325195e4caa1d6b22
Anders Cramer Nielsen <anders.cramer@me.com>
2016-12-13 09:32:37 +0100

    log out

--- admin/header.php
+++ admin/header.php
@@ -104,7 +104,7 @@ $title = str_replace('-', ' ', $title);
 									</a>
 									<ul class="dropdown-menu dropdown-usermenu pull-right">
 										<li><a class="test" href="../"><i class="fa fa-home pull-right" aria-hidden="true"></i> Go to homepage</a></li>
-										<li><a href="../logout.php" name="LOGOUT" type="submit" value="LOGOUT"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+										<li><a href="destroy" name="LOGOUT" type="submit" value="LOGOUT"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
 									</ul>
 								</li>
 								<li role="presentation">
