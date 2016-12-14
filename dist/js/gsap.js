0ea7e3a9fdfc72f605ce45eea37981a0df725601
Aschultz <andreasschultz@hotmail.com>
2016-12-14 15:50:29 +0100

    COM IT

--- dist/js/gsap.js
+++ dist/js/gsap.js
@@ -115,22 +115,22 @@ $(".search-bar .search input[value=Search], .search-bar .search input[value=Sear
 
 
 $(".label-an, .label-an").mouseenter(function(){
-            var tween = TweenMax.to('.label-an', 1, {css:{backgroundColor:'#f2f2f2', border: '1px solid #50a4b9', color:'#50a4b9' }, ease:Power4.easeOut});  
+            var tween = TweenMax.to('.label-an', 1, {css:{backgroundColor:'#f2f2f2', border: '1px solid #50a4b9', color:'#50a4b9', scale: 0.98}, ease:Power4.easeOut});  
         });
 
         $(".label-an, .label-an").mouseleave(function(){
-            var tween = TweenMax.to('.label-an', 0.750, {css:{backgroundColor:'#50a4b9', border: '1px solid white', color: 'white'}, ease:Power4.easeOut});
+            var tween = TweenMax.to('.label-an', 0.750, {css:{backgroundColor:'#303030', border: '1px solid white', color: 'white', scale: 1}, ease:Power4.easeOut});
             
         });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 $(".btn-an, .btn-an").mouseenter(function(){
-            var tween = TweenMax.to('.btn-an', 1, {css:{backgroundColor:'#f2f2f2', border: '1px solid #50a4b9', color:'#50a4b9' }, ease:Power4.easeOut});  
+            var tween = TweenMax.to('.btn-an', 1, {css:{backgroundColor:'#f2f2f2', border: '1px solid #50a4b9', color:'#50a4b9', scale: 0.98}, ease:Power4.easeOut});  
         });
 
         $(".btn-an, .btn-an").mouseleave(function(){
-            var tween = TweenMax.to('.btn-an', 0.750, {css:{backgroundColor:'#50a4b9', border: '1px solid white', color: 'white'}, ease:Power4.easeOut});
+            var tween = TweenMax.to('.btn-an', 0.750, {css:{backgroundColor:'#303030', border: '1px solid white', color: 'white', scale: 1}, ease:Power4.easeOut});
             
         });        
       
