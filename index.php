b5ae73dea8ba48f5e68d20fa7dcb76ded12ece11
Aschultz <andreasschultz@hotmail.com>
2016-12-16 12:10:05 +0100

    This is a commitment

--- index.php
+++ index.php
@@ -30,11 +30,52 @@
         </div>
 
         <div class="row row-eq-height">
-            <div class="col-sm-6">
+            <div class="col-sm-6 eq-height about">
+                <div>
 
-            </div>
+                  <!-- Nav tabs -->
+                  <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#who" aria-controls="home" role="tab" data-toggle="tab">Who are we?
+                        </a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#mission" aria-controls="profile" role="tab" data-toggle="tab">mission statement
+                        </a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#goal" aria-controls="messages" role="tab" data-toggle="tab">Goal
+                        </a>
+                    </li>
+                  </ul>
+                  <!-- Tab panes -->
+                  <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="who">
+                        <p>Cu mei case apeirian definitionem. Ullum tantas efficiantur at sit. Nec vero decore impetus no, cu sea audiam delenit nusquam, harum percipitur est at. Cum consequat mnesarchum id. In est case facer, at vel quas fuisset evertitur.
 
-            <div class="col-sm-6">
+Vis vero nihil philosophia et, at labores abhorreant eum, ex eum sumo malorum. Ad nam tation tempor, nec cu falli phaedrum. Dicit labore ne duo, eos wisi omnesque eu. Ei sententiae elaboraret nam, ei cum vidit appetere voluptatum. Vero ridens detraxit ea vix.
+                        </p>  
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="mission">
+                        <p>Vis vero nihil philosophia et, at labores abhorreant eum, ex eum sumo malorum. Ad nam tation tempor, nec cu falli phaedrum. Dicit labore ne duo, eos wisi omnesque eu. Ei sententiae elaboraret nam, ei cum vidit appetere voluptatum. Vero ridens detraxit ea vix.
+                        Cu mei case apeirian definitionem. Ullum tantas efficiantur at sit. Nec vero decore impetus no, cu sea audiam delenit nusquam, harum percipitur est at. Cum consequat mnesarchum id. In est case facer, at vel quas fuisset evertitur.
+                        </p>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="goal">
+                        <p>
+                        Lorem ipsum dolor sit amet, ei nostrud urbanitas sea, mea te nonumy impedit ancillae, ei primis sensibus mei. Ex sea autem nobis minimum. Nam ei legimus indoctum volutpat, vix ei vidisse posidonium. Mei ne mundi appareat accusata. Aperiri invidunt ex nam, porro nostrud recusabo in nam. Convenire suavitate sit an, at his dolor veniam.
+
+Cu mei case apeirian definitionem. Ullum tantas efficiantur at sit. Nec vero decore impetus no, cu sea audiam delenit nusquam, harum percipitur est at. Cum consequat mnesarchum id. In est case facer, at vel quas fuisset evertitur.
+
+Vis vero nihil philosophia et, at labores abhorreant eum, ex eum sumo malorum. Ad nam tation tempor, nec cu falli phaedrum. Dicit labore ne duo, eos wisi omnesque eu. Ei sententiae elaboraret nam, ei cum vidit appetere voluptatum. Vero ridens detraxit ea vix.
+                        </p>
+                    </div>
+                  </div>
+
+                </div>
+            </div>  
+
+            <div class="col-sm-6 eq-height quote">
 
             </div>
         </div>
