d294a5a3948f493b24e3180f1180cb5f738f5dd2
Aschultz <andreasschultz@hotmail.com>
2016-12-16 10:37:33 +0100

    Index

--- index.php
+++ index.php
@@ -18,82 +18,27 @@
             </div>
         </header>
         
-        
+        <div class="row row-eq-height">
+            <div class="col-sm-6 eq-height">
+                <img class="img-responsive" src="img/boxone.jpg">
+            </div>
+
+            <div class="col-sm-6 eq-height">
+                <h2>ABOUT</h2>
+                <p><span style="color: #51a4b9">SKYSCRAPER</span> IS A RECORD LABEL AND DIGITAL MUSIC DISTRIBUTOR<span style="color: #51a4b9">,</span> PIONEERING THE MUSIC INDUSTRY INTO THE DIGITAL AGE<span style="color: #51a4b9">.</span></p>
+            </div>
+        </div>
+
+        <div class="row row-eq-height">
+            <div class="col-sm-6">
+
+            </div>
+
+            <div class="col-sm-6">
+
+            </div>
+        </div>
         <!-- main box info -->
-        <div class="row">
-            <div class="col-sm-6 hidden-xs hidden-xs box-top-left">
-                <img src="img/boxone.jpg" alt="about skyscraper">
-            </div>
-            <div class="col-sm-6 box-top-right">
-                <div class="about-quote">
-                    <h2 class="box-headline">ABOUT</h2>
-                    <hr class="headline-underline" align="left">
-                    <br>
-                    <h4><span class="high-text">Skyscraper </span>is a record label and digital music distributor, pioneering the music industry into the digital age<span class="high-text">.</span></h4>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6 box-bottom-left">
-                <div>
-
-                  <!-- Nav tabs -->
-                  <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" class="active">
-                        <a href="#who" aria-controls="home" role="tab" data-toggle="tab">Who are we?
-                        </a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#mission" aria-controls="profile" role="tab" data-toggle="tab">mission statement
-                        </a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#goal" aria-controls="messages" role="tab" data-toggle="tab">Goal
-                        </a>
-                    </li>
-                  </ul>
-                  <!-- Tab panes -->
-                  <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane active" id="who">
-                        <p>Cu mei case apeirian definitionem. Ullum tantas efficiantur at sit. Nec vero decore impetus no, cu sea audiam delenit nusquam, harum percipitur est at. Cum consequat mnesarchum id. In est case facer, at vel quas fuisset evertitur.
-
-Vis vero nihil philosophia et, at labores abhorreant eum, ex eum sumo malorum. Ad nam tation tempor, nec cu falli phaedrum. Dicit labore ne duo, eos wisi omnesque eu. Ei sententiae elaboraret nam, ei cum vidit appetere voluptatum. Vero ridens detraxit ea vix.
-                        </p>  
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="mission">
-                        <p>Vis vero nihil philosophia et, at labores abhorreant eum, ex eum sumo malorum. Ad nam tation tempor, nec cu falli phaedrum. Dicit labore ne duo, eos wisi omnesque eu. Ei sententiae elaboraret nam, ei cum vidit appetere voluptatum. Vero ridens detraxit ea vix.
-                        Cu mei case apeirian definitionem. Ullum tantas efficiantur at sit. Nec vero decore impetus no, cu sea audiam delenit nusquam, harum percipitur est at. Cum consequat mnesarchum id. In est case facer, at vel quas fuisset evertitur.
-                        </p>
-                    </div>
-                    <div role="tabpanel" class="tab-pane" id="goal">
-                        <p>
-                        Lorem ipsum dolor sit amet, ei nostrud urbanitas sea, mea te nonumy impedit ancillae, ei primis sensibus mei. Ex sea autem nobis minimum. Nam ei legimus indoctum volutpat, vix ei vidisse posidonium. Mei ne mundi appareat accusata. Aperiri invidunt ex nam, porro nostrud recusabo in nam. Convenire suavitate sit an, at his dolor veniam.
-
-Cu mei case apeirian definitionem. Ullum tantas efficiantur at sit. Nec vero decore impetus no, cu sea audiam delenit nusquam, harum percipitur est at. Cum consequat mnesarchum id. In est case facer, at vel quas fuisset evertitur.
-
-Vis vero nihil philosophia et, at labores abhorreant eum, ex eum sumo malorum. Ad nam tation tempor, nec cu falli phaedrum. Dicit labore ne duo, eos wisi omnesque eu. Ei sententiae elaboraret nam, ei cum vidit appetere voluptatum. Vero ridens detraxit ea vix.
-                        </p>
-                    </div>
-                  </div>
-
-                </div>
-            </div>
-            <div class="col-sm-6 box-bottom-right">
-                <div class="quote-mark">
-                    <img src="icons/quote.png" alt="">
-                </div>
-                <div class="about-quote">
-                    <h4>...i give you the mausoleum of all hope and desire. I give it to you, not that you may remember time, but that you might forget it now and then.</h4>
-                </div>
-                <div class="signature">
-                    <img src="icons/placeholder_signature.svg" alt="">
-                </div>
-                <div class="jakob-ceo">
-                    <img src="icons/jakob-ceo.png" alt="">
-                </div>
-                
-            </div>
-        </div>
         <div class="row our-work">
 <!--           <div class="our-work">-->
                 <h2 class="box-headline text-center">our work</h2>
